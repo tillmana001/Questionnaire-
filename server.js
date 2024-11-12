4708636f3633c064672688796b9364d6bd99257e
allisonntillmann@gmail.com <allisonntillmann@gmail.comgit config --global user.email allisonntillmann@gmail.comgit add>
2024-11-12 11:59:30 -0500

    redone

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ async function writeResultsToGoogleSheets(scores) {
 
   // Prepare the row data, splitting out the individual question answers into separate columns
   const row = [
-    New Date(),
+    new Date(),
     scores.userName,
     scores.duration,
     Number(scores.attentionToDetail),
